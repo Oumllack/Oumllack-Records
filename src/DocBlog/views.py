4c12fae60f4797fbb2583e37f99430ed6750d802
Oumllack <122402274+Oumllack@users.noreply.github.com>
2023-01-11 03:21:49 +0700

    Add files via upload

---
+++ src/DocBlog/views.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+from django.shortcuts import render
+
+
+def index(request):
+    date = datetime.today()
+
+    return render(request, "DocBlog/index.html", context={"date": date})
